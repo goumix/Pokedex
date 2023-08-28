acea6068d07d688a11e27ec4c605f6696f352b01
Natheo Brault <nath.brault.pro@gmail.com>
2023-08-29 00:33:07 +0200

    [ADD] - searchbar

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,10 @@
 class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all.sort
+    if params[:query].present?
+      sql_subquery = "name ILIKE :query OR CAST(numero_pokedex AS TEXT) ILIKE :query"
+      @pokemons = Pokemon.all.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 
   def show
